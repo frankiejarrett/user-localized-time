f90e60edc52718cfa1001f65e29f30789fe09fa4
Frankie Jarrett <frankie@x-team.com>
2015-05-01 14:30:28 -0500

    Remove wp-admin js files

--- js/admin.js
+++
@@ -1,9 +0,0 @@
-/* globals adminpage, ult_admin */
-
-jQuery( document ).ready( function( $ ) {
-	if ( 'edit-php' === adminpage ) {
-		$( '.timestamp-wrap' ).append( ' <strong>' + ult_admin.timezone_abbr + '</strong>' );
-	} else if ( 'post-php' === adminpage || 'comment-php' === adminpage ) {
-		$( '#timestamp b' ).append( ' ' + ult_admin.timezone_abbr );
-	}
-});
